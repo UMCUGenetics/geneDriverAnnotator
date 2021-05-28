7a71b2e0f0b4d5b33bba6449c9e707c9116256a0
Luan Nguyen <nguyen.n.luan777@gmail.com>
2021-05-28 13:26:07 +0200

    Moved geneDriverAnnotator to its own repo

--- nmf/R/aaa.R
+++
@@ -1,6 +0,0 @@
-#========= Load default paths on package load =========#
-.onLoad <- function(libname, pkgname){
-
-   assign( 'MAIN_NMF_FUNCTIONS_PATH', system.file('../R/nmf.R', package='nmf'), envir=parent.env(environment()) )
-
-}
